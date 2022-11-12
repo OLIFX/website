d2d81798dca0d1aa506851a797fa1161563a3f81
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-11-12 15:53:52 -0300

    upload

---
+++ OLIFX/Database/olifx_structure.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Nov-2022 às 01:05
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `olifx`
+--
+CREATE DATABASE IF NOT EXISTS `olifx`;
+USE `olifx`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `favorite`
+--
+
+CREATE TABLE `favorite` (
+  `idUser` int(11) NOT NULL,
+  `idProduct` int(11) NOT NULL,
+  `date` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `media`
+--
+
+CREATE TABLE `media` (
+  `idProduct` int(11) NOT NULL,
+  `path` varchar(100) NOT NULL,
+  `type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `product`
+--
+
+CREATE TABLE `product` (
+  `idProduct` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` decimal(18,2) NOT NULL,
+  `date` date DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `cellphone` varchar(100) NOT NULL,
+  `fullName` varchar(255) NOT NULL,
+  `profilePic` varchar(100) DEFAULT 'default.jpg',
+  `password` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `favorite`
+--
+ALTER TABLE `favorite`
+  ADD PRIMARY KEY (`idUser`,`idProduct`),
+  ADD KEY `FK_UserFavorite` (`idUser`),
+  ADD KEY `FK_ProductFavorite` (`idProduct`);
+
+--
+-- Índices para tabela `media`
+--
+ALTER TABLE `media`
+  ADD KEY `FK_ProductMedia` (`idProduct`);
+
+--
+-- Índices para tabela `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`idProduct`),
+  ADD KEY `FK_UserProduct` (`idUser`);
+
+--
+-- Índices para tabela `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `product`
+--
+ALTER TABLE `product`
+  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `user`
+--
+ALTER TABLE `user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `favorite`
+--
+ALTER TABLE `favorite`
+  ADD CONSTRAINT `FK_ProductFavorite` FOREIGN KEY (`idProduct`) REFERENCES `product` (`idProduct`),
+  ADD CONSTRAINT `FK_UserFavorite` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`);
+
+--
+-- Limitadores para a tabela `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `FK_ProductMedia` FOREIGN KEY (`idProduct`) REFERENCES `product` (`idProduct`);
+
+--
+-- Limitadores para a tabela `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `FK_UserProduct` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
