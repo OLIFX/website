a6e6344fd65ca46627357c0ab560d675ac6d8fe8
Otávio Maldaner <otaviomaldaner571@gmail.com>
2022-11-16 09:59:59 -0300

    upload

--- OLIFX/index.php
+++ OLIFX/index.php
@@ -13,8 +13,25 @@ require_once __DIR__.'/settings/config.php';
 </head>
 <body>
     <div class="homepage-container">
-        <div class="hp-title">
-            <h1>Easy Selling Products Platform</h1>
+        <div class="homepage-util-area">
+            <div class="hp-superior-part">
+                <div class="hp-title">
+                    <h1>Easy Selling Products Platform</h1>
+                </div>
+                <div class="hp-subtitle">
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+                </div>
+            </div>
+            <div class="hp-mid-part">
+                <img src="./assets/images/city.svg" alt="" class="hp-main-image">
+            </div>
+            <div class="hp-bottom-part">
+                <div class="hp-button-space">
+                    <div class="hp-button">
+                        <p>Start Buying</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
