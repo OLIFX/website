21a80c81afc89d14491522d22d1e73ea322f905f
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2022-11-21 20:14:28 -0300

    Create account and Login auth done

--- OLIFX/page/login/index.php
+++ OLIFX/page/login/index.php
@@ -1,4 +1,19 @@
 <?php
+
+require_once "../../settings/config.php";
+
+if (isset($_POST["button"])) {
+    $user = new User();
+    $user->constructLogin($_POST["email"], $_POST["password"]);
+    
+    if ($user->authenticate()) {
+        echo "User authenticated.";
+        die();
+    } else {
+        header("location: index.php");
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +27,7 @@
   <title>OLIFX | Login</title>
 </head>
 <body>
-    <section class="form" enctype="multipart/form-data">
+    <section class="form">
         <form action="index.php" method="post" enctype="multipart/form-data">
             <h1 class="title-in-box">Login</h1>
 
