1d5764d5c1fa3c5f020aac0f0aaeb82b6a9f3e6a
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2022-11-21 23:58:42 -0300

    Home not-done

---
+++ OLIFX/page/home/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../assets/images/olifx_logo.png" type="image/png">
+    <link rel="stylesheet" href="style.css">
+    <title>OLIFX | Home</title>
+</head>
+<body>
+    <div class="container">
+        <div class="superior-part">
+            <div class="superior-elements">
+                <input type="text" class="search" placeholder="Search something...">
+                <img src="../../assets/images/default.png" alt="Default icon">
+            </div>
+        </div>
+
+        <div class="middle-part">
+            <div class="card">
+                <img src="../../assets/images/item.png" alt="Default icon">
+                <p class="card-title">This is an example of a title... Some stuff here</p>
+                <p class="card-description">This is an example of a description... Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            </div>
+
+            <div class="card">
+                <img src="../../assets/images/item.png" alt="Default icon">
+                <p class="card-title">This is an example of a title... Some stuff here</p>
+                <p class="card-description">This is an example of a description... Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            </div>
+
+            <div class="card">
+                <img src="../../assets/images/item.png" alt="Default icon">
+                <p class="card-title">This is an example of a title... Some stuff here</p>
+                <p class="card-description">This is an example of a description... Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            </div>
+
+            <div class="card">
+                <img src="../../assets/images/item.png" alt="Default icon">
+                <p class="card-title">This is an example of a title... Some stuff here</p>
+                <p class="card-description">This is an example of a description... Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            </div>
+        </div>
+
+        <div class="bottom-navigation">
+            <div class="anchor">
+                <img src="../../assets/icons/home.png" alt="Home" class="home">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
