4981cf3d9a7b7bdeeeb5c0a32c685bfb29b59a25
Luis Felipe Assmann <luis.assmann@aluno.feliz.ifrs.edu.br>
2022-11-30 09:52:02 -0300

    Changes on the home

--- OLIFX/page/home/index.php
+++ OLIFX/page/home/index.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION["idUser"])) {
 }
 
 $products = Product::findall();
-$directory = __DIR__ . "/../../database/users/";
+$directory = "../../database/users/";
 ?>
 
 <!DOCTYPE html>
